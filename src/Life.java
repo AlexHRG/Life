25cec29511f3fcd67b0fb6696bd258d4a7497918
AlexHRG <hirurg_03@ukr.net>
2017-04-15 18:14:54 +0300

    made second pass, game cicle

--- src/Life.java
+++ src/Life.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Life {
 
 	int[][] playMap;
-	int rows, columns, deathTreshold, divisionTreshold;
+	int rows, columns, deathTreshold, divisionTreshold, creaturesCounter;
 	
 	final static int FREECELL = 0;
 	final static int CREATURE = 1;
@@ -18,27 +18,92 @@ public class Life {
 		divisionTreshold = 6;
 	}
 	
+	
+	public int getCount() {
+		return creaturesCounter;
+	}
+	
+	public int getMaxCount() {
+		return rows * columns;
+	}
+	
 	public void printMap(){
 		for (int i=0; i<rows; i++) {
 			for (int j=0; j<columns; j++){
-				System.out.print(this.playMap[i][j] + "  ");
+				System.out.print(playMap[i][j] + "  ");
 			}
 			System.out.println(System.lineSeparator());
 		}
 	}
 	
 	public void setLife(int quantity){
-		int creaturesCounter = 0;
+		int counter = 0;
 		int randomRow, randomColumn;
 		
-		while (creaturesCounter < quantity) {
+		while (counter < quantity) {
 			randomRow = new Random().nextInt(rows);
 			randomColumn = new Random().nextInt(columns);
 			
-			if (playMap[randomRow][randomColumn] != CREATURE) {
+			if (playMap[randomRow][randomColumn] == FREECELL) {
 				playMap[randomRow][randomColumn] = CREATURE;
-				creaturesCounter += 1;
+				counter += 1;
 			}
 		}
+		
+		this.creaturesCounter = quantity;
+	}
+	
+	public void performDay(){
+		int neighbours = 0;
+		
+		for (int i=0; i<rows; i++) {
+			for (int j=0; j<columns; j++){
+				neighbours = countNeighbours(i, j);
+				
+				if (playMap[i][j] == FREECELL && neighbours > 2) {
+					playMap[i][j] = BORNING;
+				} else if (playMap[i][j] == CREATURE && neighbours < 2){
+					playMap[i][j] = DYING;
+				}
+			}
+		}
+	}
+	
+	public void performNight(){
+		for (int i=0; i<rows; i++) {
+			for (int j=0; j<columns; j++){
+				if (playMap[i][j] == BORNING) {
+					playMap[i][j] = CREATURE;
+					creaturesCounter += 1;
+				} else if (playMap[i][j] == DYING){
+					playMap[i][j] = FREECELL;
+					creaturesCounter -= 1;
+				}
+			}
+		}
+	}
+
+	private int countNeighbours(int i, int j) {
+		int startRow, finishRow, startColumn, finishColumn;
+		int neighbours = 0;
+		
+		startRow = (i == 0) ? i : i-1;
+		finishRow = (i == rows-1) ? i : i+1;
+		startColumn = (j == 0) ? j : j-1;
+		finishColumn = (j == columns-1) ? j : j+1;
+		
+		for (int searchRow = startRow; searchRow <= finishRow; searchRow++) {
+			for (int searchColumn = startColumn; searchColumn <= finishColumn; searchColumn++) {
+				if (playMap[searchRow][searchColumn] == CREATURE) {
+					neighbours += 1;
+				}
+			}
+		}
+		
+		if (playMap[i][j] == CREATURE) {
+			neighbours -= 1;
+		}
+		
+		return neighbours;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,27 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Life life = new Life(5, 5);
+		Life life = new Life(5, 10);
+		boolean gameOver = false;
 		
-		life.setLife(8);
+		life.setLife(20);
 		
+		System.out.println("Initial map:");
 		life.printMap();
+		
+		int counter = 0;
+		
+		while (!gameOver) {
+			life.performDay();
+			life.performNight();
+			
+			counter++;
+			
+			if (life.getCount() < 1 || life.getCount() == life.getMaxCount()) {
+				gameOver = true;
+			}
+		}
+		System.out.println("Creatures: " + life.getCount() + ", iterations: " + counter);
 	}
 
 }
